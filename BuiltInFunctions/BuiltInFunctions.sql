0cf3842adf0fc269083374612b12834773ec5f91
SimeonVSimeonov <simeonsimeonoff@icloud.com>
2019-01-22 00:53:58 +0200

    ADD ALL TASKS

--- BuiltInFunctions/BuiltInFunctions.sql
+++ BuiltInFunctions/BuiltInFunctions.sql
@@ -55,15 +55,77 @@ FROM Employees
 WHERE LEN(LastName) = 5
 
 --Task 10
-SELECT *
-FROM Employees
+SELECT EmployeeID,FirstName,LastName,Salary,
+	DENSE_RANK()OVER(PARTITION BY Salary  ORDER BY EmployeeID ) AS Rank
+FROM Employees AS R
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
 
 --Task 11
-
+SELECT * 
+FROM(
+SELECT  EmployeeID,FirstName,LastName,Salary,
+DENSE_RANK()OVER (PARTITION BY Salary  ORDER BY EmployeeID ) AS 
+Rank FROM Employees AS R
+) Employees
+WHERE Rank = 2 AND Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
 
 --Task 12
-
-
+SELECT CountryName,IsoCode
+FROM Countries
+WHERE CountryName LIKE '%A%A%A%'
+ORDER BY IsoCode
 
 --Task 13
+SELECT PeakName, RiverName,
+LOWER(PeakName + SUBSTRING(RiverName,2, LEN(RiverName)-1)) AS [MIX]
+FROM Peaks
+JOIN Rivers ON RIGHT(PeakName,1) = LEFT(RiverName,1)
+ORDER BY MIX
+
+--Task 14
+SELECT TOP(50) [Name],FORMAT(Start,' yyyy-MM-dd') AS [Start]
+FROM Games
+WHERE  DATEPART(YEAR,Start) IN (2011,2012)
+ORDER BY [Start],[Name]
+
+--Task 15
+SELECT Username,RIGHT(Email, LEN(Email) - CHARINDEX('@', Email, 1)) AS [Email Provider]
+FROM Users
+ORDER BY [Email Provider],Username
+
+--Task 16
+SELECT Username,IpAddress AS [IP Address]
+FROM Users
+WHERE IpAddress LIKE '___.1%.%.___'
+ORDER BY Username
+
+--Task 17
+SELECT [Name] AS [Game],
+CASE 
+		WHEN DATEPART(HOUR,Start) < 12
+				THEN 'Morning'
+		WHEN DATEPART(HOUR, Start) < 18 
+				THEN 'Afternoon'
+		ELSE 'Evening'
+END AS [Part Of The Day],
+CASE
+	WHEN Duration <= 3 
+		THEN 'Extra Short'
+	WHEN Duration <= 6 
+		THEN 'Short'
+	WHEN Duration > 6 
+		THEN 'Long'
+	WHEN Duration IS NULL 
+		THEN 'Extra Long'
+END AS Duration
+FROM Games
+ORDER BY [Name], Duration, [Part Of The Day]
+
+--Task 18
+SELECT ProductName,OrderDate,
+DATEADD(DAY, 3, OrderDate) AS [Pay Due],
+DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+FROM Orders 
 
